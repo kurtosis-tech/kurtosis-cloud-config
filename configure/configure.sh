dae9ff42001b1ef21ab9c4fa7ab54d5b659fd587
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-06-27 21:13:23 +0200

    configure scripts

---
+++ configure/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail # Bash "strict mode"
+
+cp "kurtosis-portal.service" "/lib/systemd/system/kurtosis-portal.service"
+TARGET="ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock"
+REPLACEMENT="ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=/root/ca.pem --tlscert=/root/server-cert.pem --tlskey=/root/server-key.pem -H=0.0.0.0:9722 -H fd:// --containerd=/run/containerd/containerd.sock"
+sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' test.txt
+
+# Finally reload
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable kurtosis-portal
+systemctl start kurtosis-portal

49ccef566be4b18203dacd18f1dc11adcf89bdd7
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-06-27 21:39:07 +0200

    update file

--- configure/configure.sh
+++ configure/configure.sh
@@ -4,7 +4,7 @@
 cp "kurtosis-portal.service" "/lib/systemd/system/kurtosis-portal.service"
 TARGET="ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock"
 REPLACEMENT="ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=/root/ca.pem --tlscert=/root/server-cert.pem --tlskey=/root/server-key.pem -H=0.0.0.0:9722 -H fd:// --containerd=/run/containerd/containerd.sock"
-sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' test.txt
+sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' /lib/systemd/system/docker.service
 
 systemctl daemon-reload
 systemctl restart docker

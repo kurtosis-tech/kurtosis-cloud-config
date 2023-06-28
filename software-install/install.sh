168695035ce17dd76f7c87d7aecce6983c901fdb
Anders Schwartz <adschwartz@users.noreply.github.com>
2023-06-28 14:25:45 +0200

    feat: add config file generation (#2)
    * wip
    
    * rename and fix
    
    * for debugging

--- software-install/install.sh
+++ software-install/install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y ca-certificates curl gnupg lsb-release
+apt-get install -y ca-certificates curl gnupg lsb-release jq
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/nullapt-get update

087c8cbde0793f4805e6f193b807f9b329ccbe41
Anders Schwartz <anders.schwartz@kurtosistech.com>
2023-06-29 00:17:37 +0200

    fix: more path

--- configure_processes.sh
+++ configure_processes.sh
@@ -7,10 +7,21 @@ fi
 
 WORK_DIR="$1"
 
+replace_values () {
+  TARGET="$1"
+  REPLACEMENT="$2"
+  CONFIG_FILE="$3"
+  sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' "$CONFIG_FILE"
+}
+
+TARGET="{WORK_DIR}"
+REPLACEMENT="$WORK_DIR"
+replace_values "$TARGET" "$REPLACEMENT" "kurtosis-portal.service"
 cp "kurtosis-portal.service" "/lib/systemd/system/kurtosis-portal.service"
+
 TARGET="ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock"
 REPLACEMENT="ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=$WORK_DIR/ca.pem --tlscert=$WORK_DIR/server-cert.pem --tlskey=$WORK_DIR/server-key.pem -H=0.0.0.0:9722 -H fd:// --containerd=/run/containerd/containerd.sock"
-sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' /lib/systemd/system/docker.service
+replace_values "$TARGET" "$REPLACEMENT" "/lib/systemd/system/docker.service"
 
 systemctl daemon-reload
 systemctl restart docker

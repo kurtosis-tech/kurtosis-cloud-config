c1e643e4395c5f7f4fb4e18f08fa535c496fad04
Laurent Luce <laurentluce49@yahoo.com>
2023-07-25 18:50:26 -0400

    Fix call to base64 and copy remote backend config to the right location

--- generate_remote_backend_config.sh
+++ generate_remote_backend_config.sh
@@ -19,11 +19,14 @@ replace_values () {
   sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' "$CONFIG_FILE"
 }
 
-ca_pem_base64=$(base64 -i ca.pem)
-client_cert_base64=$(base64 -i client-cert.pem)
-client_key=$(base64 -i client-key.pem)
+ca_pem_base64=$(base64 -w0 ca.pem)
+client_cert_base64=$(base64 -w0 client-cert.pem)
+client_key=$(base64 -w0 client-key.pem)
 
 replace_values "{ENDPOINT}" "$ENDPOINT"
 replace_values "{CA}" "$ca_pem_base64"
 replace_values "{CLIENT_CERT}" "$client_cert_base64"
 replace_values "{CLIENT_KEY}" "$client_key"
+
+mkdir -p /root/engine_config
+cp remote_backend_config.json /root/engine_config

640fd12b3736da2739dbde16565456398225e88d
Anders Schwartz <anders.schwartz@kurtosistech.com>
2024-01-11 10:25:53 -0500

    push

--- configure_logging.sh
+++ configure_logging.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Configuring cloudwatch logs: Starting"
+cp aws-ec2-logging-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
 wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 dpkg -i -E ./amazon-cloudwatch-agent.deb
 mkdir -p /usr/share/collectd/
 touch /usr/share/collectd/types.db
-cp aws-ec2-logging-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status -m ec2
 echo "Configuring cloudwatch logs: Completed"

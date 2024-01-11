05890914e9618ae68423852bd496ea6c72409882
Anders Schwartz <anders.schwartz@kurtosistech.com>
2024-01-11 12:08:05 -0500

    fix

--- configure_logging.sh
+++ configure_logging.sh
@@ -5,7 +5,7 @@ wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-
 dpkg -i -E ./amazon-cloudwatch-agent.deb
 mkdir -p /usr/share/collectd/
 touch /usr/share/collectd/types.db
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/root/aws-ec2-logging-config.json -s
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/root/kurtosis-cloud-config/aws-ec2-logging-config.json -s
 sleep 2s
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status -m ec2
 echo "Configuring cloudwatch logs: Completed"

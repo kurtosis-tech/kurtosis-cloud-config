a1a9d2d2a8fd56123cf5526fc7e00f7cc77c0537
Anders Schwartz <adschwartz@users.noreply.github.com>
2024-01-12 17:08:13 -0500

    feat: add logging and fix a bug (#13)

---
+++ configure_logging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Configuring cloudwatch logs: Starting"
+#cp aws-ec2-logging-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+mkdir -p /usr/share/collectd/
+touch /usr/share/collectd/types.db
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/root/kurtosis-cloud-config/aws-ec2-logging-config.json -s
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status -m ec2
+echo "Configuring cloudwatch logs: Completed"
--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y ca-certificates curl gnupg lsb-release jq
-
 # Docker
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/nullapt-get update
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl restart unattended-upgrades.service
 
 # Kurtosis Portal
 wget https://github.com/kurtosis-tech/kurtosis-portal/releases/download/0.0.6/kurtosis-portal_0.0.6_linux_amd64.tar.gz
@@ -17,4 +15,5 @@ rm kurtosis-portal_0.0.6_linux_amd64.tar.gz
 
 # Kurtosis CLI
 echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
+apt update
 apt-get install kurtosis-cli

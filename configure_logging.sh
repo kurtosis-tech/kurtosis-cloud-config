ae30da0a4f47286ae9b5c77a83e73fc1d6aef6d7
Anders Schwartz <anders.schwartz@kurtosistech.com>
2024-01-18 09:17:08 -0500

    feat: change log group name

--- configure_logging.sh
+++ configure_logging.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-
 set -v
 
+missing_var=false
+if [ -z "$1" ]; then
+  echo "Error: No name defined as first argument"
+  missing_var=true
+fi
+
+if [ "$missing_var" = true ]; then
+  exit 1
+fi
+
+NAME="$1"
+CONFIG_FILE="aws-ec2-logging-config.json"
+
+
+replace_values () {
+  TARGET=$1
+  REPLACEMENT=$2
+  sed -i 's!'"$TARGET"'!'"$REPLACEMENT"'!g' "$CONFIG_FILE"
+}
+
 echo "Configuring cloudwatch logs: Starting"
+replace_values "{KURTOSIS_NAME}" "$NAME"
 #cp aws-ec2-logging-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
 wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 dpkg -i -E ./amazon-cloudwatch-agent.deb

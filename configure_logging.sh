4cd5da90a5f4caf6be5ff097fdc04d883321f5fe
Laurent Luce <laurentluce49@yahoo.com>
2024-01-25 19:07:08 -0500

    Reduce the number of retries to 7 from 10.

--- configure_logging.sh
+++ configure_logging.sh
@@ -26,8 +26,8 @@ replace_values () {
 echo "Configuring cloudwatch logs: Starting"
 replace_values "{KURTOSIS_NAME}" "$NAME"
 #cp aws-ec2-logging-config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
-retry 10 wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-retry 10 dpkg -i -E ./amazon-cloudwatch-agent.deb
+retry 7 wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+retry 7 dpkg -i -E ./amazon-cloudwatch-agent.deb
 mkdir -p /usr/share/collectd/
 touch /usr/share/collectd/types.db
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/root/kurtosis-cloud-config/aws-ec2-logging-config.json -s
--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ set -euv
 
 # Docker and Kurtosis CLI
 mkdir -m 0755 -p /etc/apt/keyrings
-retry 10 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+retry 7 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
-retry 10 apt-get update
-retry 10 apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kurtosis-cli
+retry 7 apt-get update
+retry 7 apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kurtosis-cli
 systemctl restart unattended-upgrades.service
 
 # Kurtosis Portal
-retry 10 wget https://github.com/kurtosis-tech/kurtosis-portal/releases/download/0.0.6/kurtosis-portal_0.0.6_linux_amd64.tar.gz
+retry 7 wget https://github.com/kurtosis-tech/kurtosis-portal/releases/download/0.0.6/kurtosis-portal_0.0.6_linux_amd64.tar.gz
 tar zxvf kurtosis-portal_0.0.6_linux_amd64.tar.gz
 rm kurtosis-portal_0.0.6_linux_amd64.tar.gz

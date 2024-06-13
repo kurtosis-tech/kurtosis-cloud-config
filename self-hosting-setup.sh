f36c775ec14b9a1d39cd8645f853c95ec95502fe
Laurent Luce <laurentluce49@yahoo.com>
2024-06-13 13:32:28 -0400

    Get Nginx config from the main branch

--- self-hosting-setup.sh
+++ self-hosting-setup.sh
@@ -45,5 +45,5 @@ fi
 mkdir /etc/apache2
 htpasswd -b -c /etc/apache2/.htpasswd $username $password
 
-retry 7 curl -fsSL https://raw.githubusercontent.com/kurtosis-tech/kurtosis-cloud-config/laurent/self-cloud-hosting/self-hosting-nginx.conf -o /etc/nginx/nginx.conf
+retry 7 curl -fsSL https://raw.githubusercontent.com/kurtosis-tech/kurtosis-cloud-config/main/self-hosting-nginx.conf -o /etc/nginx/nginx.conf
 systemctl reload nginx

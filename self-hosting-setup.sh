b4bc3963277de8cab420d5afe0019ffdc131a587
Laurent Luce <laurentluce49@yahoo.com>
2024-06-13 10:43:11 -0400

    Add basic auth

--- self-hosting-setup.sh
+++ self-hosting-setup.sh
@@ -2,6 +2,8 @@
 set -euv
 
 domain=$1
+username=$2
+password=$3
 
 function retry {
   local retries=$1
@@ -28,7 +30,7 @@ retry 7 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dea
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
 retry 7 apt-get update
-retry 7 apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kurtosis-cli nginx
+retry 7 apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kurtosis-cli nginx apache2-utils
 systemctl restart unattended-upgrades.service
 
 kurtosis analytics enable
@@ -39,6 +41,9 @@ else
     echo "Failed to start the engine"
     exit 1
 fi
+
+mkdir /etc/apache2
+htpasswd -b -c /etc/apache2/.htpasswd $username $password
 
 retry 7 curl -fsSL https://raw.githubusercontent.com/kurtosis-tech/kurtosis-cloud-config/laurent/self-cloud-hosting/self-hosting-nginx.conf -o /etc/nginx/nginx.conf
 systemctl reload nginx

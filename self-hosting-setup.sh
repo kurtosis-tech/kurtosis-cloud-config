3bf6a2439c9931a0f7e0162f9c9b823de55deb81
Laurent Luce <laurentluce49@yahoo.com>
2024-06-13 14:28:44 -0400

    Cleanup

--- self-hosting-setup.sh
+++ self-hosting-setup.sh
@@ -25,14 +25,18 @@ function retry {
   return 0
 }
 
+# Install Kurtosis CLI
 mkdir -m 0755 -p /etc/apt/keyrings
 retry 7 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
+
+# Install Docker and Nginx
 retry 7 apt-get update
 retry 7 apt-get install -y ca-certificates curl gnupg lsb-release jq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kurtosis-cli nginx apache2-utils
 systemctl restart unattended-upgrades.service
 
+# Start Kurtosis engine with domain set
 kurtosis analytics enable
 kurtosis engine start --domain $domain
 if [ $? -eq 0 ]; then
@@ -42,8 +46,8 @@ else
     exit 1
 fi
 
+# Configure Nginx
 mkdir /etc/apache2
 htpasswd -b -c /etc/apache2/.htpasswd $username $password
-
 retry 7 curl -fsSL https://raw.githubusercontent.com/kurtosis-tech/kurtosis-cloud-config/main/self-hosting-nginx.conf -o /etc/nginx/nginx.conf
 systemctl reload nginx
